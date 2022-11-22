f94a4c53ffe5bfb00f928d14cdfdd050f7f7cf6e
Lucas Kaczmarski <lucaskacz1@gmail.com>
2022-11-22 19:46:59 +0100

    Add Hardhat extension

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 const Home = props => {
+
+    let[contractData, setContractData] = useState("");
+
+    const connectContractDealer = async() => {
+
+    }
+
+
     return (
         <Card className={classes.home}>
             <h1>Welcome</h1>
             <p>{props.currentAccount}</p>
+            <button onClick={connectContractDealer}></button>
         </Card>
     );
 };
