165f10efe32c0087573d12d5a0f895e58cfbe0db
SnyDrive25 <sunilgouvi@gmail.com>
2022-11-23 18:59:55 +0100

    fichiers sol de Lucas

--- app/contracts/Dealer.sol
+++ app/contracts/Dealer.sol
@@ -3,6 +3,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 
+
 contract Dealer is Ownable {
 
     event NewCar(uint carID, string vin_number, string model);
@@ -14,19 +15,23 @@ contract Dealer is Ownable {
 
     Car[] public cars;
 
+    address [] public addresses;
+
     mapping (uint => address) public carToOwner;
     mapping (address => uint) public ownerCarCount;
 
-    //address dealer = 
+    /*address dealer = "0x77DbD1ddF6d9BfaB2aD5e76986A0628BB09B8Ae9";
 
     function _defineDealer (address _dealer)public onlyOwner returns(address){
         return _dealer;
-    }
+    }*/
 
-    function _createCar(string memory _vin_number, string memory _model) public{
+    function _createCar(string memory _vin_number, string memory _model) public onlyOwner{
         //require(msg.sender == )
         cars.push(Car(_vin_number, _model));
         uint id = cars.length -1;
+        carToOwner[id] = msg.sender;
+        ownerCarCount[msg.sender]++;
         emit NewCar(id, _vin_number, _model);
         //carToOwner[id] = msg.sender;
         //ownerCarCount[msg.sender]++;
--- app/contracts/DealerOwnership.sol
+++ app/contracts/DealerOwnership.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.8.9;
 import "./Dealer.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+
 contract DealerOwnership is Dealer, ERC721{
 
     constructor() ERC721("Dealer", "IDC") public { }
+
+    mapping(uint => address) carApprovals;
+
+    function add_concess(address _address)public onlyOwner{
+        addresses.push(_address);
+    }
+
+    function liste_concess() public returns(address [] memory){
+        return addresses;
+    }
 
     function balanceOf(address _owner) public override view returns (uint256 _balance) {
         return ownerCarCount[_owner];
@@ -15,23 +26,33 @@ contract DealerOwnership is Dealer, ERC721{
         return carToOwner[_tokenId];
     }
 
-    function _transfer(address _from, address _to, uint256 _tokenId) internal virtual override{  
+    function _transfer(address _from, address _to, uint256 _tokenId) internal virtual override{
+        ownerCarCount[_to]++;
+        ownerCarCount[_from]--;
+        carToOwner[_tokenId] = _to;  
         emit Transfer(_from, _to, _tokenId);
     }
     
     function transfer(address _to, uint256 _tokenId) public  {
+        address temp = 0x0000000000000000000000000000000000000000;
+        require(carToOwner[_tokenId] != temp);
         _transfer(msg.sender, _to, _tokenId);
     }
 
-    /*
-    function approve(address _to, uint256 _tokenId) public {
-
+    
+    function approve(address _to, uint256 _tokenId) public override {
+        carApprovals[_tokenId] = _to;
+        emit Approval(msg.sender, _to, _tokenId);
     }
 
     function takeOwnership(uint256 _tokenId) public {
-
+        require(carApprovals[_tokenId] == msg.sender);
+        address owner = ownerOf(_tokenId);
+        _transfer(owner, msg.sender, _tokenId);
     }
-    */
     
-    
+    function transferOwnership(address newOwner) public override virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
 }
