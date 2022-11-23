6ea5d66c41970b6872e151603196cc2073a4aeb4
Al3xGROS <al.gros@sfr.fr>
2022-11-23 16:56:49 +0100

    Add Service_contract

---
+++ app/contracts/Service.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Dealer.sol";
+
+
+
+contract Service is DealerOwnership {
+
+    event NewService(uint serviceID, uint carID, string description, bool result);
+
+    struct Service{
+        uint carID;
+        string description;
+        bool result;
+    }
+
+    Service[] public services;
+
+    mapping (uint => uint) public serviceToCar;
+    mapping (uint => adress) public serviceToOwner;
+    mapping (address => uint) public ownerServiceCount;
+
+    function _createService (uint memory _carID, string memory _description, bool memory _result) public onlyOwner{
+        services.push(Service(_carID, _description, _result));
+        uint id = services.length -1;
+        serviceToCar[id] = _carID;
+        ownerServiceCount[msg.sender]++;
+        emit NewService(id, _carID, _description, _result);
+    }
+}
